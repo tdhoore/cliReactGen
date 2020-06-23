8a2c9a805bca4431250d5fce0c2216153a0f1153
Tim D'hoore <timdhoore@gmail.com>
2020-06-23 19:22:31 +0200

    fixed the root reducer and configure store

--- templates/create-project/src/redux/rootReducer.ts
+++ templates/create-project/src/redux/rootReducer.ts
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import { ReduxState } from "./types";
 
-const createRootReducer = (history) =>
+const createRootReducer = (history: any) =>
   combineReducers({
     router: connectRouter(history),
     //nameReducer: nameReducer,
